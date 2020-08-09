d5962e3f4be7f5d6e5209525d3fc978ffd31f3f0
Christin Mikolai <mail@christinmikolai.de>
2020-08-09 19:28:51 +0200

    adjust nav component

--- components/nav.js
+++ components/nav.js
@@ -5,33 +5,35 @@ const Nav = ({ categories }) => {
   return (
     <div>
       <nav className="navbar-container">
-        <div className="navbar-left">
-          <ul className="navbar-nav">
-            <li>
-              <Link href="/">
-                <a>Christin Mikolai</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div className="container">
+          <div className="navbar-left">
+            <ul className="navbar-nav">
+              <li>
+                <Link href="/">
+                  <a>Christin Mikolai</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className="navbar-right">
-          <ul className="navbar-nav">
-            {categories.map(category => {
-              return (
-                <li key={category.id}>
-                  <Link as={`/category/${category.id}`} href="/category/[id]">
-                    <a className="">{category.name}</a>
-                  </Link>
-                </li>
-              )
-            })}
-            <li key='aboutme'>
-              <Link as={`/aboutme`} href="/aboutme">
-                <a className="">About Me</a>
-              </Link>
-            </li>
-          </ul>
+          <div className="navbar-right">
+            <ul className="navbar-nav">
+              {categories.map(category => {
+                return (
+                  <li key={category.id}>
+                    <Link as={`/category/${category.id}`} href="/category/[id]">
+                      <a className="">{category.name}</a>
+                    </Link>
+                  </li>
+                )
+              })}
+              <li key='aboutme'>
+                <Link as={`/aboutme`} href="/aboutme">
+                  <a className="">About Me</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
