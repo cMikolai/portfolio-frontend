60a1d71e77a3d44a2d2db5c1a4a6db6fbaf692cc
Christin Mikolai <mail@christinmikolai.de>
2020-08-09 19:28:42 +0200

    add footer component

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div id="footer-container">
+        <div className="footer-text text-left">2020</div>
+        <div className="footer-text text-right">❤️ cMikolai</div>
+        {/* <ul className="footer-links">
+          <li>
+            <Link href="/">
+              <a>Home Link Test</a>
+            </Link>
+          </li>
+        </ul> */}
+      </div>
+    </div>
+  )
+}
+
+export default Footer
